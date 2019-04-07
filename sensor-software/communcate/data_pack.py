e4ee73f0c4a32bbcd3384e3b02dbc47e29a7ab4f
old_wang <854266726@qq.com>
2019-04-07 14:54:13 +0800

    code review

--- sensor-software/communcate/data_pack.py
+++ sensor-software/communcate/data_pack.py
@@ -2,9 +2,10 @@
 
 import minimalmodbus
 
-
 def singletonDecorator(cls, *args, **kwargs):
-    """定义一个单例装饰器"""
+    """
+    the singleton decorator.
+    """
     instance = {}
 
     def wrapperSingleton(*args, **kwargs):
@@ -15,16 +16,19 @@ def singletonDecorator(cls, *args, **kwargs):
     return wrapperSingleton
 
 @singletonDecorator
-class dataPack():
-    
+class DataPack():
+    """
+    sensor's data package
+    """
+
     def __init__(self, name):
         self.__name = name
           
-    def setData(self,data):
-        self.__setIlluminance(data)
-        self.__setHumidity(data)
-        self.__setTemperature(data)
-        self.__setWindspeed(data)
+    def set_data(self, data):
+        self.__set_illuminance(data)
+        self.__set_humidity(data)
+        self.__set_temperature(data)
+        self.__set_windspeed(data)
     
     def illuminance(self):
         return self.__illuminance
@@ -40,54 +44,38 @@ class dataPack():
     
     def name(self):
         return self.__name
+
+    # TODO(Gehaha)    
+    # def __address(self):
+    #     pass
+
+    # def __set_functionCode(self):
+    #     pass
     
-    def __address(self):
-        pass
+    # def __datalen(self):
+    #     pass
     
-    def __setFunctionCode(self):
-        pass
-    
-    def __datalen(self):
-        pass
-    
-    
-    def __setCrc(self):
-        pass
+    # def __set_crc(self):
+    #     pass
         
-    def __setIlluminance(self,data):
+    def __set_illuminance(self, data):
         self.__illuminance = minimalmodbus._twoByteStringToNum(data[3:5],
                                                                numberOfDecimals = 0,signed = False)
-    def __setTemperature(self,data):
+    def __set_temperature(self, data):
         self.__temperature = minimalmodbus._twoByteStringToNum(data[5:7],
                                                                numberOfDecimals = 2,signed = False)
       
-    def __setHumidity(self,data):
+    def __set_humidity(self, data):
         self.__humidity = minimalmodbus._twoByteStringToNum(data[7:9],
                                                             numberOfDecimals = 2,signed = False)
       
-    def __setWindspeed(self,data):
+    def __set_windspeed(self, data):
         self.__windspeed = minimalmodbus._twoByteStringToNum( data[9:11],
                                                              numberOfDecimals = 2,signed = False)
 
-#def test():
-#    package = dataPack("sensor of modbus")
-#    package.setData("\x01\x03\x08\x00Ä\t?\x13Ö\x00\x00áû")
-#    
-#    print (package.illuminance())
-#    print (package.temperature())
-#    print (package.humidity())
-#    print (package.windspeed())
-#    print ('\n')
-#    
-#    package2 = dataPack()
-#    package2.setData("\x01\x03\x08\x00Ä\tB\x13½\x00\x00ü-")
-#    print (package.illuminance())
-#    print (package.temperature())
-#    print (package.humidity())
-#    print (package.windspeed())
-
-#TEST = False
-#
-#if TEST:
-#    test() 
-    
+    def print_data(self):
+        print("illuminace: ", self.__illuminance)
+        print("temperature:", self.__temperature)
+        print("humidity:   ", self.__humidity)
+        print("windspeed:  ", self.__windspeed)
+        print()
